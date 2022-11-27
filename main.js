f42a79e4023fc1d70d6a488779ee1243c8b84d9d
kinxori <116584982+kinxori@users.noreply.github.com>
2022-11-27 14:21:29 -0600

    javascript added with animations

--- main.js
+++ main.js
@@ -9,16 +9,10 @@ const hyperlinks = divIcons.querySelectorAll('a');
 
 function addAnimationDelay(node, index){
 
-
-    node.style.opacity = 1;
     node.classList.add('scale-delay');
     node.style = "--animationDelay:" + index;
     
     console.log(node + index)
-
-    
-    
-    
 }
 
 hyperlinks.forEach(addAnimationDelay)
